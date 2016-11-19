12cbb5ec8cd7ca17683107e539a14ee00a4b32de
alexey192 <alexey192@gmail.com>
2016-11-19 23:25:03 +0300

    DB fix

---
+++ HackRussiaDemo/HackRussiaDemo/Models/startSessionRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HackRussiaDemo.Models
+{
+    public class startSessionRequest
+    {
+        public string email { get; set; }
+    }
+}
