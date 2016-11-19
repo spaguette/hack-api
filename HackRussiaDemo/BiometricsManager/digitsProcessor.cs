11fe2bfba7587ffd7ddffb55859c649434644736
alexey192 <alexey192@gmail.com>
2016-11-19 19:12:12 +0300

    Всё пуском

---
+++ HackRussiaDemo/BiometricsManager/digitsProcessor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiometricsManager
+{
+    public static class digitsProcessor
+    {
+        public static List<int> getRandomDigits()
+        {
+            int length = 5;
+
+            var result = new List<int>();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                result.Add(rnd.Next(10));
+            }
+
+            return result;
+        }
+
+        public static List<string> digitHumanizer(List<int> digits)
+        {
+            var result = new List<string>();
+
+            foreach(var digit in digits)
+            {
+                switch(digit)
+                {
+                    case 0:
+                        result.Add("ноль");
+                        break;
+                    case 1:
+                        result.Add("один");
+                        break;
+                    case 2:
+                        result.Add("два");
+                        break;
+                    case 3:
+                        result.Add("три");
+                        break;
+                    case 4:
+                        result.Add("четыре");
+                        break;
+                    case 5:
+                        result.Add("пять");
+                        break;
+                    case 6:
+                        result.Add("шесть");
+                        break;
+                    case 7:
+                        result.Add("семь");
+                        break;
+                    case 8:
+                        result.Add("восемь");
+                        break;
+                    case 9:
+                        result.Add("девять");
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ HackRussiaDemo/HackRussiaDemo/Models/createUserRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using BiometricsManager.Models;
+
+namespace HackRussiaDemo.Models
+{
+    public class createUserRequest
+    {
+        public string email { get; set; }
+
+        public string authToken { get; set; }
+
+        public AudioRecord voiceSamples { get; set; }
+    }
+}
