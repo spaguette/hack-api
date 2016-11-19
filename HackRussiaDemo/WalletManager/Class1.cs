1a9aaf2f96b69fe1b8737f6c05942399c9a18cd4
faldown <faldown@yandex.ru>
2016-11-19 19:50:44 +0500

    init

--- HackRussiaDemo/WalletManager/Class1.cs
+++ HackRussiaDemo/WalletManager/Class1.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Collections.Specialized;
 
 namespace WalletManager
 {
     public class Class1
     {
+        static void Main(string[] args)
+        {
+            string url = "http://site.com/";
+
+            using (var webClient = new WebClient())
+            {
+                // Создаём коллекцию параметров
+                var pars = new NameValueCollection();
+
+                // Добавляем необходимые параметры в виде пар ключ, значение
+                pars.Add("format", "json");
+
+                // Посылаем параметры на сервер
+                // Может быть ответ в виде массива байт
+                var response = webClient.UploadValues(url, pars);
+            }
+        }
     }
 }
